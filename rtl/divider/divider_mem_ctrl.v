65aec7b24a70e83ed20c8738f3ff0b33788c043e
akrish21 <aravindh.plk@gmail.com>
2016-04-23 17:32:14 -0400

    adding write addr gen for divider and testbench for divider mem ctrl

--- rtl/divider/divider_mem_ctrl.v
+++ rtl/divider/divider_mem_ctrl.v
@@ -3,95 +3,182 @@ input  wire         clk,
 input  wire         reset,
 input  wire         enable,
 input  wire         div_done,
-output reg [15:0]   sc_mem_rdaddr,
+output reg [15:0]   sc_mem_rd_addr1,
+output reg [15:0]   sc_mem_rd_addr2,
+output reg [15:0]   sc_mem_wt_addr,
 output reg          sc_mem_rd_en,
-output reg          sc_mem_rd_done
+output reg          sc_mem_wt_en,
+output reg          sc_mem_rd_done,
+output reg          sc_mem_wt_done
 );
 
-//FSM STATE PARAMETERS
+//ALL FSM STATE PARAMETERS
 parameter
-  IDLE        = 3'b000,
-  FIRST_RD    = 3'b001,
-  WAITFORDIV  = 3'b010,
-  NEXT_RD     = 3'b011,
-  COMPLETE    = 3'b100;
+  IDLE           = 3'b000,
+  FIRST_RD       = 3'b001,
+  WAITFORDIV_RD  = 3'b010,
+  NEXT_RD        = 3'b011,
+  COMPLETE_RD    = 3'b100,
+  WAITFORDIV_WT  = 3'b101,
+  WRITE          = 3'b110,
+  COMPLETE_WT    = 3'b111;
   
   
-reg   [2:0]   state;
-reg   [2:0]   next_state;
-reg   [15:0]  next_sc_mem_rdaddr;
+reg   [2:0]   rd_state;
+reg   [2:0]   wt_state;
+reg   [2:0]   next_rd_state;
+reg   [2:0]   next_wt_state;
+reg   [15:0]  next_sc_mem_rd_addr1;
+reg   [15:0]  next_sc_mem_rd_addr2;
+reg   [15:0]  next_sc_mem_wt_addr;
 reg           next_sc_mem_rd_en;
+reg           next_sc_mem_wt_en;
 reg           next_sc_mem_rd_done;
+reg           next_sc_mem_wt_done;
 reg   [6:0]   rd_line_count;
+reg   [6:0]   wt_line_count;
+reg   [6:0]   next_rd_line_count;
+reg   [6:0]   next_wt_line_count;
 
 //Moore output updates
 always @(posedge clk) begin
 
   if(reset) begin
-    state               <= IDLE;
-	sc_mem_rdaddr       <= 16'd0;
+    rd_state            <= IDLE;
+	wt_state            <= IDLE;
 	sc_mem_rd_en        <= 1'b0;
+	sc_mem_wt_en        <= 1'b0;
 	sc_mem_rd_done      <= 1'b0;
+	sc_mem_wt_done      <= 1'b0;
+	rd_line_count       <= 7'd0;
+	wt_line_count       <= 7'd0;
   end
   else begin
-    state               <= next_state;
-	sc_mem_rdaddr       <= next_sc_mem_rdaddr;
+    rd_state            <= next_rd_state;
+	wt_state            <= next_wt_state;
+	sc_mem_rd_addr1     <= next_sc_mem_rd_addr1;
+	sc_mem_rd_addr2     <= next_sc_mem_rd_addr2;
+	sc_mem_wt_addr      <= next_sc_mem_wt_addr;
 	sc_mem_rd_en        <= next_sc_mem_rd_en;
+	sc_mem_wt_en        <= next_sc_mem_wt_en;
 	sc_mem_rd_done      <= next_sc_mem_rd_done;
+	sc_mem_wt_done      <= next_sc_mem_wt_done;
+	rd_line_count       <= next_rd_line_count;
+	wt_line_count       <= next_wt_line_count;
   end
 end
 
-//FSM LOGIC FOR DIVISION CORE
+//FSM LOGIC TO READ FROM SCRATCH MEM
 always @(*) begin
 
-case(state)
+case(rd_state)
 
-
+    //IDLE state after reset and read completion
     IDLE:begin
         next_sc_mem_rd_done  =  1'b0;
 		next_sc_mem_rd_en    =  1'b0;
-		next_sc_mem_rdaddr   =  16'd0;
-		rd_line_count        =  7'd0;
+		next_rd_line_count   =  7'd0;
 		
 		if(enable) begin
-		   next_state  =   FIRST_RD;
+		   next_rd_state  =   FIRST_RD;
 		end
 		else begin
-		   next_state  =   IDLE;
+		   next_rd_state  =   IDLE;
 		end
 	end
 	
+	//State entered only for the first read
 	FIRST_RD:begin
-		next_sc_mem_rdaddr   =  16'd64;
+		next_sc_mem_rd_addr1 =  16'd64;
+		next_sc_mem_rd_addr2 =  16'd65;
 		next_sc_mem_rd_en    =  1'b1;
-		rd_line_count        =  7'd1;
-		next_state           =  WAITFORDIV;
+		next_rd_line_count   =  7'd1;
+		next_rd_state        =  WAITFORDIV_RD;
 	end
 	
-	WAITFORDIV:begin
+	//State to wait while divider is running 
+	WAITFORDIV_RD:begin
 		next_sc_mem_rd_en    =  1'b0;
 		
-		if(div_done && (rd_line_count < 7'd64)) begin
-		   next_state  =  NEXT_RD;
+		if(div_done && (rd_line_count < 7'd62)) begin
+		   next_rd_state  =  NEXT_RD;
 		end
-		else if(div_done && (rd_line_count > 7'd64)) begin
-		   next_state  =  COMPLETE;
+		else if(div_done && (rd_line_count > 7'd62)) begin
+		   next_rd_state  =  COMPLETE_RD;
 		end
 		else begin
-		   next_state  = WAITFORDIV;
+		   next_rd_state  =  WAITFORDIV_RD;
 		end
 	end
 	
+	//State to read from scratch after 1st read
 	NEXT_RD:begin
-	    next_sc_mem_rdaddr  =  sc_mem_rdaddr + 2;
-		next_sc_mem_rd_en   =  1'b1;
-		rd_line_count       =  rd_line_count + 2;
-		next_state          =  WAITFORDIV;
+	    next_sc_mem_rd_addr1 =  sc_mem_rd_addr1 + 2;
+		next_sc_mem_rd_addr2 =  sc_mem_rd_addr2 + 2;
+		next_sc_mem_rd_en    =  1'b1;
+		next_rd_line_count   =  rd_line_count + 2;
+		next_rd_state        =  WAITFORDIV_RD;
 	end
 	
-	COMPLETE:begin
+	//All cdf values read and division is complete
+    //on last read cdf values
+	COMPLETE_RD:begin
 	    next_sc_mem_rd_done  =  1'b1;
-		next_state           =  IDLE;
+		next_rd_state        =  IDLE;
+	end
+endcase
+end
+
+
+
+//FSM LOGIC TO WRITE TO SCRATCH MEM
+always @(*) begin
+
+case(wt_state)
+
+    //IDLE state after reset and write completion
+    IDLE:begin
+        next_sc_mem_wt_done  =  1'b0;
+		next_sc_mem_wt_en    =  1'b0;
+		next_sc_mem_wt_addr  =  16'd128;
+		next_wt_line_count   =  7'd0;
+		
+		if(enable) begin
+		   next_wt_state  =   WAITFORDIV_WT;
+		end
+		else begin
+		   next_wt_state  =   IDLE;
+		end
+	end
+
+	//State to wait while divider is running 
+	WAITFORDIV_WT:begin
+		next_sc_mem_wt_en    =  1'b0;
+		
+		if(div_done && (wt_line_count < 7'd63)) begin
+		   next_wt_state  =  WRITE;
+		end
+		else if(div_done && (wt_line_count >= 7'd63)) begin
+		   next_wt_state  =  COMPLETE_WT;
+		end
+		else begin
+		   next_wt_state  =  WAITFORDIV_WT;
+		end
+	end
+	
+	//State to generate write en and wr addr
+	WRITE:begin
+		next_sc_mem_wt_addr  =  sc_mem_wt_addr + 1;
+		next_sc_mem_wt_en    =  1'b1;
+		next_wt_line_count   =  wt_line_count + 1;
+		next_wt_state        =  WAITFORDIV_WT;
+	end
+	
+    //All div values written and division is complete
+    //on last read cdf values
+	COMPLETE_WT:begin
+	    next_sc_mem_wt_done  =  1'b1;
+		next_wt_state        =  IDLE;
 	end
 endcase
 end
---
+++ tb/divider/divider_mem_ctrl_tb.v
@@ -0,0 +1,66 @@
+`include "rtl/divider/divider_mem_ctrl.v"
+
+module divider_mem_ctrl_tb ;
+
+  reg         clk;
+  reg         reset;
+  reg         enable;
+  reg         div_done;
+  wire [15:0] sc_mem_rd_addr1;
+  wire [15:0] sc_mem_rd_addr2;
+  wire [15:0] sc_mem_wt_addr;
+  wire        sc_mem_rd_en;
+  wire        sc_mem_wt_en;
+  wire        sc_mem_rd_done;
+  wire        sc_mem_wt_done;
+  
+
+divider_mem_ctrl d_memctrl1(
+  .clk              (clk),
+  .reset            (reset),
+  .enable           (enable),
+  .div_done         (div_done),
+  .sc_mem_rd_addr1  (sc_mem_rd_addr1),
+  .sc_mem_rd_addr2  (sc_mem_rd_addr2),
+  .sc_mem_wt_addr   (sc_mem_wt_addr),
+  .sc_mem_rd_done   (sc_mem_rd_done),
+  .sc_mem_wt_done   (sc_mem_wt_done),
+  .sc_mem_rd_en     (sc_mem_rd_en),
+  .sc_mem_wt_en     (sc_mem_wt_en)
+  );
+  
+  initial
+  begin
+  
+  clk = 1;
+  reset = 1;
+  div_done = 0;
+  enable = 0;
+  #5 reset = 0;
+  #5 enable = 1;
+  #10 enable = 0;
+  end
+  
+  //Toggle divider_done to test reading
+  //of all cdf values from scratch
+  always
+  #10 div_done = ~div_done;
+  
+  always 
+  #5 clk = !clk;
+  
+  initial
+  begin
+  
+  $dumpfile ("divider_mem_ctrl.vcd");
+  $dumpvars;
+  end
+  
+  initial
+  #3000 $finish;
+  
+endmodule
+  
+  
+
+  
