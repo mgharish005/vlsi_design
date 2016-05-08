cc51963d011238a852be721d1c6a67bf82bce51b
Kaushik Gopalakrishnan <disciple.of.einstein@gmail.com>
2016-05-08 17:14:30 -0500

    Fix ompilation errors, update state transitions and control outputs. Removed unnecessary parts of datapath developed without control knowledge

--- rtl/cdf/cdf_control.v
+++ rtl/cdf/cdf_control.v
@@ -5,9 +5,9 @@
 // cdf_control.v
 //--------------------------------------------
 
-module cdf_control(clk, reset, cdf_start_in, read_first_value, scratch_mem_read_ready, cdf_computation_done, cdf_done);
+module cdf_control(clk, reset, cdf_start_in, read_first_value, scratch_mem_read_ready, cdf_computation_done, read_next_value, cdf_done);
 
-parameter WAIT = 0, START = 1, READ_READY = 2, COMPUTE = 3, WRITE = 4, IMAGE_DONE = 5;
+parameter WAIT = 0, START = 1, EMPTY1 = 2, READ_READY = 3, COMPUTE = 4, WRITE = 5, IMAGE_DONE = 6;
 
 // inputs
 input clk;
@@ -15,6 +15,10 @@ input reset;
 input cdf_start_in;
 
 // outputs
+output read_first_value;
+output scratch_mem_read_ready;
+output cdf_computation_done;
+output read_next_value;
 output cdf_done;
 
 reg cdf_start;
@@ -24,14 +28,20 @@ reg cdf_computation_done;
 reg cdf_computation_done_out;
 reg scratch_mem_read_ready;
 reg scratch_mem_read_ready_out;
+reg read_first_value;
+reg read_first_value_out;
+reg read_next_value;
+reg read_next_value_out;
 
 wire cdf_start_in;
 
 // Variables
-reg [1:0] state;
-reg nextState;
+reg [2:0] state = WAIT;
+reg [2:0] nextState;
 reg [7:0] count;
-
+reg [7:0] cdf_count;
+reg reset_counter;
+reg reset_cdf_counter;
 
 // Flop inputs
 always @(posedge clk)
@@ -45,6 +55,7 @@ begin
 	cdf_done <= cdf_done_out;
 	read_first_value <= read_first_value_out;
 	cdf_computation_done <= cdf_computation_done_out;
+	read_next_value <= read_next_value_out;
 	scratch_mem_read_ready <= scratch_mem_read_ready_out;
 end	
 
@@ -68,14 +79,14 @@ end
 // Overall cdf counter
 always @(posedge clk) begin
 	if (reset) begin
-		cdf_counter <= 0;
+		cdf_count <= 0;
 	end
 	else begin
 		if (reset_cdf_counter == 1'b1) begin
-			cdf_counter <= 8'd0;
+			cdf_count <= 8'd0;
 		end		
 		else begin
-			cdf_counter <= cdf_counter + 1;
+			cdf_count <= cdf_count + 1;
 		end	
 	end	
 
@@ -90,23 +101,28 @@ always @(*)
 		cdf_computation_done_out = 1'b0;
 		cdf_done_out = 1'b0;
 		scratch_mem_read_ready_out = 1'b0;
+		read_next_value_out = 1'b0;
 	
 		case (state)
 			WAIT : 
 			begin
-				if (cdf_start) 
+				if (cdf_start) begin
 					nextState = START;
+					reset_counter = 1'b1;
+					reset_cdf_counter = 1'b1;
+				end	
 				else
 					nextState = WAIT;
 			end
 
 			START :
 			begin
-				if (cdf_counter == 8'd0) begin
+				if (cdf_count == 8'd0) begin
 					read_first_value_out = 1'b1;
 				end
 				else begin
-					read_next_value = 1'b1;
+					read_next_value_out = 1'b1;
+					read_first_value_out = 1'b0;
 				end		
 				nextState = EMPTY1;
 			end
@@ -123,7 +139,6 @@ always @(*)
 				scratch_mem_read_ready_out = 1'b1;
 				nextState = COMPUTE;
 				reset_counter = 1'b0;
-				end	
 			end
 			
 			COMPUTE :
@@ -156,7 +171,8 @@ always @(*)
 						reset_counter = 1'b0;
 					end			
 				end							 			 
-
+			end
+			
 			IMAGE_DONE :
 			begin
 				reset_counter = 1'b1;
--- rtl/cdf/cdf_datapath.v
+++ rtl/cdf/cdf_datapath.v
@@ -5,13 +5,11 @@
 // cdf_datapath.v
 //--------------------------------------------
 
-module cdf_datapath (clk,reset,scratchmem_input1,scratchmem_input2,read_first_value_in,scratch_mem_read_ready_in,cdf_computation_done_in,cdf_done_in,scratchmem_output);
+module cdf_datapath (clk, reset, scratchmem_input1, scratchmem_input2, read_first_value_in, scratch_mem_read_ready_in, cdf_computation_done_in, read_next_value_in, cdf_done_in, scratchmem_output, WE, WriteAddress, WriteBus, ReadAddress1, ReadAddress2);
 
 // inputs
 input clk;
 input reset;
-input [127:0] ReadBus1;
-input [127:0] ReadBus2;
 
 // outputs
 output WE;
@@ -19,22 +17,32 @@ output [15:0] WriteAddress;
 output [127:0] WriteBus;
 output [15:0] ReadAddress1;
 output [15:0] ReadAddress2;
+output [127:0] scratchmem_output;
 
 input [127:0] scratchmem_input1;
-input [127:0] scracthmem_input2;
+input [127:0] scratchmem_input2;
 input read_first_value_in;
+input read_next_value_in;
 input scratch_mem_read_ready_in;
 input cdf_computation_done_in;
 input cdf_done_in;
-		
-// outputs
-output [127:0] scratchmem_output;
 
+// output reg declarations
+reg WE;
+reg [15:0] WriteAddress;
+reg [127:0] WriteBus;
+reg [15:0] ReadAddress1;
+reg [15:0] ReadAddress2;
+reg read_first_value;
+reg scratch_mem_read_ready;
+reg cdf_computation_done;
+reg cdf_done;
+reg read_next_value;
 reg [127:0] scratchmem_output;
 reg [127:0] scratchmem_data1;
 reg [127:0] scratchmem_data2;
-wire [31:0] histogram[8];
-reg [31:0] cdf[8];
+wire [31:0] histogram[0:7];
+reg [31:0] cdf[0:7];
 
 // flop the inputs
 always @(posedge clk)
@@ -43,6 +51,7 @@ begin
 		scratchmem_data1 <= 0;
 		scratchmem_data2 <= 0;
 		read_first_value <= 0;
+		read_next_value <= 0;
 		cdf_computation_done <= 0;
 		cdf_done <= 0;
 	end
@@ -50,6 +59,7 @@ begin
 		scratchmem_data1 <= scratchmem_input1;
 		scratchmem_data2 <= scratchmem_input2;
 		read_first_value <= read_first_value_in;
+		read_next_value <= read_next_value_in;
 		cdf_computation_done <= cdf_computation_done_in;
 		cdf_done <= cdf_done_in;
 	end
@@ -62,7 +72,7 @@ begin
 		ReadAddress1 <= 16'd400;	// some arbitrary address
 		ReadAddress2 <= 16'd401;
 		WriteAddress <= 0;
-		WriteBus < = 0;			
+		WriteBus <= 0;			
 end
 				
 // Use scratchmem inputs as histogram. Requires efficient way of computing cdf					
@@ -85,12 +95,12 @@ begin
 	end
 	else if (cdf_computation_done) begin
 		WriteAddress <= WriteAddress + 16'd1;
-		WriteBus <= WriteBus_gen;
+		//WriteBus <= WriteBus_gen;
 		WE <= 1'b1;
 	end		
 	else if (cdf_done) begin
 		WriteAddress <= WriteAddress + 16'd1;
-		WriteBus <= cdf_min;
+		//WriteBus <= cdf_min;
 		WE <= 1'b1;
 	end
 	else begin
@@ -100,20 +110,10 @@ begin
 	end
 end		
 
-
-// Keep memory of cdf_prev from the previous cycle
-always @(posedge clk)
-begin
-
-end
-
-
-
-
 // Compute cdf. Store the last cdf to be re-used in next iteration.
 // We don't have to worry about carry_out while adding, because it automatically stored within the 32-bit result. 
 // The largest value of histogram = 8294400 which can be represented in 24 bits. 
-always @(posedge clk)
+/*always @(posedge clk)
 begin
 	if (scratch_mem_read_ready) begin
 		cdf[0] <= cdf_prev + histogram[0];
@@ -127,9 +127,9 @@ begin
 	end
 	else if (
 	
-end						
+end*/						
 						
-						
+endmodule						
 						
 						
 						
