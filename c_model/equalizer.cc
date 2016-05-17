02f8265be75a879f16bda9922c3081aef7e5e135
Harish M Gopalakrishnan <mgharish005@Harishs-MacBook-Pro.local>
2016-05-16 21:38:00 -0700

    Update to dump scratch mem in correct format

--- c_model/equalizer.cc
+++ c_model/equalizer.cc
@@ -126,23 +126,24 @@ uint32_t* draw_histogram(uint8_t** f, uint32_t l, uint32_t m, uint32_t n)
 
     for (int i = 0 ; i < two_pow_l; i++)
             *(h+i) = 0; 
+    printf("histogram---------");
     for(int i = 0; i<n; i++)
 	 {
          printf("\n"); 
-    		for(int j = m-1; j>=0; j--)
-    		{
+    	 for(int j = m-1; j>=0; j--)
+    	 {
             uint8_t offset_temp; 
             uint8_t waddr_in_memory; 
 
             h_index = *(*(f+i)+j) & two_pow_l; 
             offset_temp  = ((h_index) & 3); 
 
-            printf("%x", h_index); 
+            printf("%d ", h_index); 
             if(h_index >= 1<<l)
                 printf("FATAL Issue - due to h_index >= 2^l"); 
             *(h + h_index) = *(h + h_index) + 1 ; 
 
-
+		
           //printf("offset_Temp = %0d\n", offset_temp); 
           //printf("h_index/4 = %0d\n", h_index/4); 
             waddr_in_memory = (h_index>>2); 
@@ -185,7 +186,7 @@ uint32_t* compute_cdf(uint32_t* h, uint32_t l)
     pFile_scratchmem_dump_for_divider = fopen("scratchmem_dump_for_divider.txt", "w"); 
 
     uint32_t* cdf;
-    uint32_t min = l<<1;
+    uint32_t min = 55555;
     uint8_t waddr_in_memory; 
     
     uint32_t two_pow_l = (1<<l) -1 ;  
@@ -195,31 +196,43 @@ uint32_t* compute_cdf(uint32_t* h, uint32_t l)
 	 	printf("Unable to allocate memory for histogram"); 	
 
 
-    for(int i = 0; i < two_pow_l; i++)
-    { 
-        for(int j = 0; j <= i ; j++)
-                *(cdf + i) = *(cdf+i) + *(h+j);  
-       
+    for(int i = 0; i <= two_pow_l; i++)
+    {
+        if(i) 
+            *(cdf + i) = *(cdf+i - 1) + *(h+i);  
+        else
+            *(cdf) = *h;  
         waddr_in_memory = i / 4;  
         fprintf(pFile_cdf_waddr, "%032x\n", waddr_in_memory); 
-        fprintf(pFile_cdf_wdata, "%032xd\n", *(cdf + i)); 
+        fprintf(pFile_cdf_wdata, "%032x\n", *(cdf + i)); 
 
         //find min
             if(*(cdf+i) < min && *(cdf+i) > 0)
                 min = *(cdf + i);  
     } 
     *(cdf + (l<<1)) = min; 
+
     fprintf(pFile_cdf_wdata, "%d\n", *(cdf + (l<<1))); 
    
     printf("tow_pow_l/4 = %d", (two_pow_l)); 
-    for(int i = 0; i< (two_pow_l+1)/4; i = i + 1)
+    printf("min [cdf_compute]= %d", (min)); 
+    printf("two_pow_l + 1 /4 = %d", (two_pow_l+1)/4); 
+
+ // printf("hisotgrammmm------"); 
+ // for(int j = 0 ; j < two_pow_l + 1; j++)
+ //     printf("%08x\n", *(h+j));
+ // printf("------------------"); 
+
+    int i,j; 
+    for( i = 0,  j = 0; i< (two_pow_l+1)/4; i = i + 1, j = j + 4)
     {
-        fprintf(pFile_scratchmem_dump_for_cdf, "%032x%032x%032x%032x\n", *(h +i), *(h+i+1), *(h+i+2), *(h+i+3) ); 
-        fprintf(pFile_scratchmem_dump_for_divider, "%032x%032x%032x%032x\n", *(h +i), *(h+i+1), *(h+i+2), *(h+i+3) ); 
+      //printf("hist i %d\n",i);
+        fprintf(pFile_scratchmem_dump_for_cdf, "%08x%08x%08x%08x\n", *(h +j), *(h+j+1), *(h+j+2), *(h+j+3) ); 
+        fprintf(pFile_scratchmem_dump_for_divider, "%08x%08x%08x%08x\n", *(h +j), *(h+j+1), *(h+j+2), *(h+j+3) ); 
     }
-    for(int j = 0; j< (two_pow_l+1)/4; j = j + 1)
+    for(i=0, j = 0; i< (two_pow_l+1)/4; i++, j = j + 4)
     {
-        fprintf(pFile_scratchmem_dump_for_divider, "%032x%032x%032x%032x\n", *(cdf +j), *(cdf+j+1), *(cdf+j+2), *(cdf+j+3) ); 
+        fprintf(pFile_scratchmem_dump_for_divider, "%08x%08x%08x%08x\n", *(cdf +j), *(cdf+j+1), *(cdf+j+2), *(cdf+j+3) ); 
     }
 	 printf("cdf computed successfully \n"); 
     fclose(pFile_cdf_wdata); 
@@ -229,17 +242,15 @@ uint32_t* compute_cdf(uint32_t* h, uint32_t l)
     return cdf; 
 }
 
-uint8_t** compute_output(uint32_t* cdf, uint8_t** input_image, uint32_t m, uint32_t n, uint32_t l)
+uint8_t** compute_output(uint32_t* cdf, uint8_t** input_image, uint32_t m, uint32_t n, uint32_t l, uint32_t cdf_min)
 {
 	 pFile_divider_scratch_wdata = fopen("divider_scratch_wdata.txt", "w"); 
 	 pFile_divider_scratch_waddr = fopen("divider_scratch_waddr.txt", "w"); 
 
-    uint32_t cdf_min;  
     uint8_t input_image_local;  
     uint8_t** output_image; 
     uint8_t* cdf_order_output_image; 
     uint32_t two_pow = 1 <<l;
-    cdf_min = *(cdf + two_pow); 
     
     float factor = 1.0; 
     factor = (float)(two_pow-1)/((n*m)-cdf_min); 
@@ -269,21 +280,21 @@ uint8_t** compute_output(uint32_t* cdf, uint8_t** input_image, uint32_t m, uint3
         *(cdf_order_output_image + i) = (*(cdf+i) - cdf_min)*factor;   
         if(i % 4 == 0)
         { 
-            fprintf(pFile_divider_scratch_wdata, "%032x%032x%032x%032x\n", *(cdf_order_output_image+(i)),*(cdf_order_output_image+i+1),*(cdf_order_output_image+i+2),*(cdf_order_output_image+i+3));  
+            fprintf(pFile_divider_scratch_wdata, "%08x%08x%08x%08x\n", *(cdf_order_output_image+(i)),*(cdf_order_output_image+i+1),*(cdf_order_output_image+i+2),*(cdf_order_output_image+i+3));  
         } 
         else if(i % 4 == 1)
         {
-            fprintf(pFile_divider_scratch_wdata, "%032x%032x%032x%032x\n", *(cdf_order_output_image+i-1),*(cdf_order_output_image+i),*(cdf_order_output_image+i+1),*(cdf_order_output_image+i+2));  
+            fprintf(pFile_divider_scratch_wdata, "%08x%08x%08x%08x\n", *(cdf_order_output_image+i-1),*(cdf_order_output_image+i),*(cdf_order_output_image+i+1),*(cdf_order_output_image+i+2));  
 
         }
         else if(i % 4 == 2)
         {
-            fprintf(pFile_divider_scratch_wdata, "%032x%032x%032x%032x\n", *(cdf_order_output_image+i-2),*(cdf_order_output_image+i-1),*(cdf_order_output_image+i),*(cdf_order_output_image+i+1));  
+            fprintf(pFile_divider_scratch_wdata, "%08x%08x%08x%08x\n", *(cdf_order_output_image+i-2),*(cdf_order_output_image+i-1),*(cdf_order_output_image+i),*(cdf_order_output_image+i+1));  
 
         }
         else if(i % 4 == 3)
         {
-            fprintf(pFile_divider_scratch_wdata, "%032x%032x%032x%032x\n", *(cdf_order_output_image+i-3),*(cdf_order_output_image+i-2),*(cdf_order_output_image+i-1),*(cdf_order_output_image+i));  
+            fprintf(pFile_divider_scratch_wdata, "%08x%08x%08x%08x\n", *(cdf_order_output_image+i-3),*(cdf_order_output_image+i-2),*(cdf_order_output_image+i-1),*(cdf_order_output_image+i));  
         } 
         
         fprintf(pFile_divider_scratch_waddr, "%032x\n", i/4); 
@@ -335,7 +346,7 @@ int main(int argc, char *argv[])
     cdf_min = *(cdf + two_pow); 
 
     //4. compute g
-    g = compute_output(cdf, f, M, N, l); 
+    g = compute_output(cdf, f, M, N, l, cdf_min); 
     return 0; 
 
 }
